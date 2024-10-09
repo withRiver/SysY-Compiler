1b7f7b5afb9d2f2e3980d2019e708fd6f6de9f81
李鸿瑞 <2100012963@stu.pku.edu.cn>
2024-10-09 16:08:13 +0800

    a

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <fstream>
 #include "AST.h"
+#include "koopa.h"
 
 using namespace std;
 
@@ -15,6 +16,20 @@ using namespace std;
 // 看起来会很烦人, 于是干脆采用这种看起来 dirty 但实际很有效的手段
 extern FILE *yyin;
 extern int yyparse(unique_ptr<BaseAST> &ast);
+
+// 访问raw program
+void Visit(const koopa_raw_program_t& program);
+// 访问 raw slice
+void Visit(const koopa_raw_slice_t &slice);
+// 访问函数
+void Visit(const koopa_raw_function_t &func);
+// 访问基本块
+void Visit(const koopa_raw_basic_block_t &bb);
+// 访问指令
+void Visit(const koopa_raw_value_t &value);
+// 访问对应类型指令
+void Visit(const koopa_raw_return_t &ret);
+void Visit(const koopa_raw_integer_t &integer);
 
 int main(int argc, const char *argv[]) {
   // 解析命令行参数. 测试脚本/评测平台要求你的编译器能接收如下参数:
@@ -45,5 +60,146 @@ int main(int argc, const char *argv[]) {
   // 将IR输出到stdout
   cout << irStr;
 
+  const char* str = irStr.c_str();
+
+  // 解析字符串 str, 得到 Koopa IR 程序
+  koopa_program_t program;
+  koopa_error_code_t ret = koopa_parse_from_string(str, &program);
+  assert(ret == KOOPA_EC_SUCCESS);  // 确保解析时没有出错
+  // 创建一个 raw program builder, 用来构建 raw program
+  koopa_raw_program_builder_t builder = koopa_new_raw_program_builder();
+  // 将 Koopa IR 程序转换为 raw program
+  koopa_raw_program_t raw = koopa_build_raw_program(builder, program);
+  // 释放 Koopa IR 程序占用的内存
+  koopa_delete_program(program);
+
+  // 处理 raw program
+  // 使用 for 循环遍历函数列表
+  for (size_t i = 0; i < raw.funcs.len; ++i) {
+    // 正常情况下, 列表中的元素就是函数, 我们只不过是在确认这个事实
+    // 当然, 你也可以基于 raw slice 的 kind, 实现一个通用的处理函数
+    assert(raw.funcs.kind == KOOPA_RSIK_FUNCTION);
+    // 获取当前函数
+    koopa_raw_function_t func = (koopa_raw_function_t) raw.funcs.buffer[i];
+    // 进一步处理当前函数
+    for (size_t j = 0; j < func->bbs.len; ++j) {
+      assert(func->bbs.kind == KOOPA_RSIK_BASIC_BLOCK);
+      koopa_raw_basic_block_t bb = (koopa_raw_basic_block_t) func->bbs.buffer[j];
+      // 进一步处理当前基本块
+      koopa_raw_value_t value = (koopa_raw_value_t) bb->insts.buffer[0];
+      // 示例程序中, 你得到的 value 一定是一条 return 指令
+      assert(value->kind.tag == KOOPA_RVT_RETURN);
+      // 于是我们可以按照处理 return 指令的方式处理这个 value
+      // return 指令中, value 代表返回值
+      koopa_raw_value_t ret_value = value->kind.data.ret.value;
+      // 示例程序中, ret_value 一定是一个 integer
+      assert(ret_value->kind.tag == KOOPA_RVT_INTEGER);
+      // 于是我们可以按照处理 integer 的方式处理 ret_value
+      // integer 中, value 代表整数的数值
+      int32_t int_val = ret_value->kind.data.integer.value;
+      // 示例程序中, 这个数值一定是 0
+      assert(int_val == 0);
+    }
+  }
+
+  // 处理完成, 释放 raw program builder 占用的内存
+  // 注意, raw program 中所有的指针指向的内存均为 raw program builder 的内存
+  // 所以不要在 raw program 处理完毕之前释放 builder
+  koopa_delete_raw_program_builder(builder);
+
   return 0;
+}
+
+// 访问 raw program
+void Visit(const koopa_raw_program_t &program) {
+  // 执行一些其他的必要操作
+  // ...
+  // 访问所有全局变量
+  Visit(program.values);
+  // 访问所有函数
+  Visit(program.funcs);
+}
+
+// 访问 raw slice
+void Visit(const koopa_raw_slice_t &slice) {
+  for (size_t i = 0; i < slice.len; ++i) {
+    auto ptr = slice.buffer[i];
+    // 根据 slice 的 kind 决定将 ptr 视作何种元素
+    switch (slice.kind) {
+      case KOOPA_RSIK_FUNCTION:
+        // 访问函数
+        Visit(reinterpret_cast<koopa_raw_function_t>(ptr));
+        break;
+      case KOOPA_RSIK_BASIC_BLOCK:
+        // 访问基本块
+        Visit(reinterpret_cast<koopa_raw_basic_block_t>(ptr));
+        break;
+      case KOOPA_RSIK_VALUE:
+        // 访问指令
+        Visit(reinterpret_cast<koopa_raw_value_t>(ptr));
+        break;
+      default:
+        // 我们暂时不会遇到其他内容, 于是不对其做任何处理
+        assert(false);
+    }
+  }
+}
+
+// 访问函数
+void Visit(const koopa_raw_function_t &func) {
+  // 执行一些其他的必要操作
+  // ...
+  // 访问所有基本块
+  Visit(func->bbs);
+}
+
+// 访问基本块
+void Visit(const koopa_raw_basic_block_t &bb) {
+  // 执行一些其他的必要操作
+  // ...
+  // 访问所有指令
+  Visit(bb->insts);
+}
+
+// 访问指令
+void Visit(const koopa_raw_value_t &value) {
+  /*
+  struct koopa_raw_value_data {
+  /// Type of value.
+  koopa_raw_type_t ty;
+  /// Name of value, null if no name.
+  const char *name;
+  /// Values that this value is used by.
+  koopa_raw_slice_t used_by;
+  /// Kind of value.
+  koopa_raw_value_kind_t kind;
+};
+  */
+  // 根据指令类型判断后续需要如何访问
+  const auto &kind = value->kind;
+  switch (kind.tag) {
+    case KOOPA_RVT_RETURN:
+      // 访问 return 指令
+      Visit(kind.data.ret);
+      break;
+    case KOOPA_RVT_INTEGER:
+      // 访问 integer 指令
+      Visit(kind.data.integer);
+      break;
+    default:
+      // 其他类型暂时遇不到
+      assert(false);
+  }
+}
+
+// 访问对应类型指令的函数定义略
+// 视需求自行实现
+// ...
+void Visit(const koopa_raw_return_t &ret) {
+  Visit(ret.value);
+}
+
+void Visit(const koopa_raw_integer_t &integer) {
+  int32_t int_val = integer.value;
+  assert(int_val == 0);
 }
