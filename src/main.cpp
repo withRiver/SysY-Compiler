4ef83df6d9d3174408f142fbf272388b43df8cf0
李鸿瑞 <2100012963@stu.pku.edu.cn>
2024-10-09 19:03:30 +0800

    lv1: generate RISCV of int main

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,9 @@ void Visit(const koopa_raw_value_t &value);
 void Visit(const koopa_raw_return_t &ret);
 void Visit(const koopa_raw_integer_t &integer);
 
+// 全局变量riscV_str, 用于存储编译得到的RSICV代码
+string riscV_str("");
+
 int main(int argc, const char *argv[]) {
   // 解析命令行参数. 测试脚本/评测平台要求你的编译器能接收如下参数:
   // compiler 模式 输入文件 -o 输出文件
@@ -45,20 +48,20 @@ int main(int argc, const char *argv[]) {
 
   // 调用 parser 函数, parser 函数会进一步调用 lexer 解析输入文件的
   unique_ptr<BaseAST> ast;
-  auto ret = yyparse(ast);
-  assert(!ret);
+  auto yyparse_ret = yyparse(ast);
+  assert(!yyparse_ret);
 
   string irStr("");
   ast->DumpIR(irStr);
 
-  // 将IR输出到output
+  // 输出文件流outFile
   std::ofstream outFile(output);
   assert(outFile);
-  outFile << irStr;
-  outFile.close();
+  //outFile << irStr;
+  //outFile.close();
 
   // 将IR输出到stdout
-  cout << irStr;
+  //cout << irStr;
 
   const char* str = irStr.c_str();
 
@@ -74,39 +77,17 @@ int main(int argc, const char *argv[]) {
   koopa_delete_program(program);
 
   // 处理 raw program
-  // 使用 for 循环遍历函数列表
-  for (size_t i = 0; i < raw.funcs.len; ++i) {
-    // 正常情况下, 列表中的元素就是函数, 我们只不过是在确认这个事实
-    // 当然, 你也可以基于 raw slice 的 kind, 实现一个通用的处理函数
-    assert(raw.funcs.kind == KOOPA_RSIK_FUNCTION);
-    // 获取当前函数
-    koopa_raw_function_t func = (koopa_raw_function_t) raw.funcs.buffer[i];
-    // 进一步处理当前函数
-    for (size_t j = 0; j < func->bbs.len; ++j) {
-      assert(func->bbs.kind == KOOPA_RSIK_BASIC_BLOCK);
-      koopa_raw_basic_block_t bb = (koopa_raw_basic_block_t) func->bbs.buffer[j];
-      // 进一步处理当前基本块
-      koopa_raw_value_t value = (koopa_raw_value_t) bb->insts.buffer[0];
-      // 示例程序中, 你得到的 value 一定是一条 return 指令
-      assert(value->kind.tag == KOOPA_RVT_RETURN);
-      // 于是我们可以按照处理 return 指令的方式处理这个 value
-      // return 指令中, value 代表返回值
-      koopa_raw_value_t ret_value = value->kind.data.ret.value;
-      // 示例程序中, ret_value 一定是一个 integer
-      assert(ret_value->kind.tag == KOOPA_RVT_INTEGER);
-      // 于是我们可以按照处理 integer 的方式处理 ret_value
-      // integer 中, value 代表整数的数值
-      int32_t int_val = ret_value->kind.data.integer.value;
-      // 示例程序中, 这个数值一定是 0
-      assert(int_val == 0);
-    }
-  }
+  Visit(raw);
 
   // 处理完成, 释放 raw program builder 占用的内存
   // 注意, raw program 中所有的指针指向的内存均为 raw program builder 的内存
   // 所以不要在 raw program 处理完毕之前释放 builder
   koopa_delete_raw_program_builder(builder);
 
+  cout << riscV_str << endl;
+  outFile << riscV_str << endl;
+
+  outFile.close();
   return 0;
 }
 
@@ -114,6 +95,7 @@ int main(int argc, const char *argv[]) {
 void Visit(const koopa_raw_program_t &program) {
   // 执行一些其他的必要操作
   // ...
+  riscV_str += "\t.text\n";
   // 访问所有全局变量
   Visit(program.values);
   // 访问所有函数
@@ -148,7 +130,12 @@ void Visit(const koopa_raw_slice_t &slice) {
 // 访问函数
 void Visit(const koopa_raw_function_t &func) {
   // 执行一些其他的必要操作
-  // ...
+  riscV_str += "\t.globl ";
+  string name = string(func->name + 1);
+  riscV_str += name;
+  riscV_str += "\n";
+  riscV_str += name;
+  riscV_str += ":\n";
   // 访问所有基本块
   Visit(func->bbs);
 }
@@ -156,7 +143,6 @@ void Visit(const koopa_raw_function_t &func) {
 // 访问基本块
 void Visit(const koopa_raw_basic_block_t &bb) {
   // 执行一些其他的必要操作
-  // ...
   // 访问所有指令
   Visit(bb->insts);
 }
@@ -197,9 +183,18 @@ void Visit(const koopa_raw_value_t &value) {
 // ...
 void Visit(const koopa_raw_return_t &ret) {
   Visit(ret.value);
+  riscV_str += "\tret\n";
 }
 
 void Visit(const koopa_raw_integer_t &integer) {
   int32_t int_val = integer.value;
-  assert(int_val == 0);
-}
+  riscV_str += "\tli a0, " + to_string(int_val) + "\n";
+} 
+
+/*
+.text
+  .globl main
+main:
+  li a0, 0
+  ret
+*/
