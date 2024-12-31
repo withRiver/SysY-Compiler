c375640a3e62fa912f576d3a4f8016789e2037bd
李鸿瑞 <2100012963@stu.pku.edu.cn>
2024-12-31 13:11:38 +0800

    My compiler

--- src/main.cpp
+++ src/main.cpp
@@ -82,6 +82,37 @@ int main(int argc, const char *argv[]) {
     // 注意, raw program 中所有的指针指向的内存均为 raw program builder 的内存
     // 所以不要在 raw program 处理完毕之前释放 builder
     koopa_delete_raw_program_builder(builder);
+  } else if(string(mode) == "-perf") {
+    // 先得到koopa-ir并储存在str中
+    stringstream ss;
+    streambuf* cout_buf = cout.rdbuf();
+    cout.rdbuf(ss.rdbuf());
+    ast->DumpIR();
+    string irStr = ss.str();
+    cout.rdbuf(cout_buf);
+    const char* str = irStr.c_str();
+    // 与 "-riscv" 相同
+    // 解析字符串 str, 得到 Koopa IR 程序
+    koopa_program_t program;
+    koopa_error_code_t ret = koopa_parse_from_string(str, &program);
+    assert(ret == KOOPA_EC_SUCCESS);  // 确保解析时没有出错
+    // 创建一个 raw program builder, 用来构建 raw program
+    koopa_raw_program_builder_t builder = koopa_new_raw_program_builder();
+    // 将 Koopa IR 程序转换为 raw program
+    koopa_raw_program_t raw = koopa_build_raw_program(builder, program);
+    // 释放 Koopa IR 程序占用的内存
+    koopa_delete_program(program);
+
+    cout.rdbuf(outFile.rdbuf()); 
+    // 处理 raw program
+    Visit(raw);
+    cout.rdbuf(cout_buf);
+    // 输出到标准输出，便于调试
+    Visit(raw);
+    // 处理完成, 释放 raw program builder 占用的内存
+    // 注意, raw program 中所有的指针指向的内存均为 raw program builder 的内存
+    // 所以不要在 raw program 处理完毕之前释放 builder
+    koopa_delete_raw_program_builder(builder);    
   }
 
   outFile.close();
